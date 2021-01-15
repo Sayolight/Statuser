33be84c9a8034ba9aef87b6809d07834e5e385f4
Artemiy Sayolight <47282388+Sayolight@users.noreply.github.com>
2021-01-15 15:52:20 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
-import time
 import json
+import random
+import os.path
+import time
+
 import requests
+from PIL import Image
+from dotenv import load_dotenv
 from telethon import functions
 from telethon.sync import TelegramClient
+from telethon.tl.functions.channels import GetFullChannelRequest
+from telethon.tl.functions.messages import GetHistoryRequest
 from telethon.tl.functions.photos import DeletePhotosRequest
-from telethon.tl.types import InputPhoto
-from PIL import Image
-
-import os
-from dotenv import load_dotenv
+from telethon.tl.types import InputPhoto, PeerChannel
 
 load_dotenv()
 
@@ -17,64 +20,87 @@ print('Sayo.Statuser by Sayolight')
 api_id = int(os.getenv('API_ID'))
 api_hash = os.getenv('API_HASH')
 strg = os.getenv('JSON_STORAGE')
-
+channel = int(os.getenv('CHANNEL'))
 
 if not strg:
     newHeaders = {'Content-type': "application/json; charset=utf-8"}
 
-    response = requests.post('https://jsonstorage.net/api/items/',
-                             data=json.dumps({'status': ''}),
-                             headers=newHeaders)
-
-    print(response.json())
+    link = f'https://jsonbox.io/sayostatuser_{random.randint(1000000000, 2000000000)}/'
+    response = requests.post(link,
+                            data=json.dumps({'status': ''}),
+                            headers=newHeaders)
+    print(link+response.json()['_id'])
 
 with TelegramClient('user', api_id, api_hash) as client:
-
     def update(s):
-        p = client.get_profile_photos('me')[0]
-        client(DeletePhotosRequest(
-            id=[InputPhoto(
-                id=p.id,
-                access_hash=p.access_hash,
-                file_reference=p.file_reference
-            )]
-        ))
+        p = client.get_profile_photos('me')
+        if p:
+            client(DeletePhotosRequest(
+                id=[InputPhoto(
+                    id=p[0].id,
+                    access_hash=p[0].access_hash,
+                    file_reference=p[0].file_reference
+                )]
+            ))
         ava = Image.open("photo.png")
         st = Image.open(f"img/{s}.png")
-
-        ava.paste(st, (0, -1), st)
-
+        width, height = ava.size
+        w, h = st.size
+        ava.paste(st, (int((width-w)/2), int((height-h)/2)), st)
         ava.save('status.png')
         client(functions.photos.UploadProfilePhotoRequest(
             file=client.upload_file('status.png'),
         ))
 
+
     while True:
         try:
-            for message in client.get_messages('me', limit=1):
-                msg = message.message.split()
+            if strg:
+                response = requests.get(f'{strg}')
+                status = response.json()['status']
+                if status and os.path.isfile(f'img/{status}.png'):
+                    update(status)
+                newHeaders = {'Content-type': "application/json; charset=utf-8"}
 
-                if strg:
-                    response = requests.get(f'https://jsonstorage.net/api/items/{strg}')
-                    status = response.json()['status']
-                    if status:
-                        update(status)
-                        newHeaders = {'Content-type': "application/json; charset=utf-8"}
+                response = requests.put(f'{strg}',
+                                        data=json.dumps({'status': ''}),
+                                        headers=newHeaders)
 
-                        response = requests.put(f'https://jsonstorage.net/api/items/{strg}',
-                                                 data=json.dumps({'status': ''}),
-                                                 headers=newHeaders)
+            msgs = client.get_messages(channel, limit=1)
 
-                if msg[0] == "$upd":
-                    client.download_profile_photo('me', "photo.png")
-                    client.edit_message(message, 'Обновлено.')
+            if msgs[0].message:
+                cmd = msgs[0].message.lower().strip().split()
+            else:
+                cmd = None
 
-                if msg[0] == "$s":
-                    try:
-                        update(msg[1])
-                        message.delete()
-                    except:
-                        client.edit_message(message, 'Произошла ошибка.')
+            if cmd[0] == "help":
+                client.edit_message(msgs[0], 'Thank You for using Statuser!\n\n' +
+                                    'Commands list:\n' +
+                                    '**s [STATUSNAME]** - set **[STATUSNAME].png** to avatar status\n')
+            if cmd[0] == "s":
+                channel_entity = client.get_entity(PeerChannel(channel))
+                channel_info = client(GetFullChannelRequest(channel_entity))
+                pinned_msg_id = channel_info.full_chat.pinned_msg_id
+
+                if pinned_msg_id is not None:
+                    posts = client(GetHistoryRequest(
+                        channel_entity,
+                        limit=1,
+                        offset_date=None,
+                        offset_id=pinned_msg_id + 1,
+                        max_id=0,
+                        min_id=0,
+                        add_offset=0,
+                        hash=0
+                    ))
+                    client.download_media(posts.messages[0], "photo.png")
+                    if os.path.isfile(f'img/{cmd[1]}.png'):
+                        update(cmd[1])
+                        client.edit_message(msgs[0], "Обновлено!")
+                        time.sleep(1)
+                        msgs[0].delete()
+                    else:
+                        client.edit_message(msgs[0], f'Can\'t find **{cmd[1]}.png**')
         except:
             pass
         time.sleep(10)
